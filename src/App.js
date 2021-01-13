97443b53fc5dbe109854a09dfe535ac9154347f2
Fabiano Garcia <fabianojp06@gmail.com>
2021-01-13 08:06:18 -0300

    13012021

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
-import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro"
+import "./assets/App.css";
+import "./assets/index.css";
 
 class App extends Component {
+
+  constructor() {
+    super();  
+    this.state = {
+      notas: []
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado);    
+  }
+
   render(){
+    
   return (
-    <section>
-    <FormularioCadastro/>
-    <ListaDeNotas/>
+    <section className="conteudo">
+    <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+    <ListaDeNotas notas={this.state.notas}/> 
   </section>
   );
  }
