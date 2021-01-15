8cb156685eb9b088361ad5576c80797d5d39ea55
Fabiano Garcia <fabianojp06@gmail.com>
2021-01-15 08:16:18 -0300

    15012021_2

--- andamento.js
+++
@@ -1,8 +0,0 @@
-continuação curso;
-
-https://cursos.alura.com.br/category/front-end/reactjs
-
-https://cursos.alura.com.br/course/react-ciclo-de-vida/task/77135
-https://github.com/alura-cursos/1841-react-observable/tree/aula2
-
-aula 02 criando lista de categorias
